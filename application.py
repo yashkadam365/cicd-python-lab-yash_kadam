cdad8bfa849c80e03bfd44b9544614398276828d
Yash Kadam <yash.kadam3605@gmail.com>
2026-03-31 12:43:12 +0530

    Update version message to 2.0 in hello_world

--- application.py
+++ application.py
@@ -4,6 +4,6 @@ application = Flask(__name__)
 @application.route('/')
 
 def hello_world():
-  return "<h1>CI/CD Pipeline is Live!</h1><p>Version 1.0: Initial Deployment.</p>"
+  return "<h1>CI/CD Pipeline is Live!</h1><p>Version 2.0 🚀</p>"
 if __name__ == '__main__':
   application.run(debug=True)
